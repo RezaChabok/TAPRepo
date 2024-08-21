9183c89845c95bc5702dc2b78fab9d7581b25c2a
RezaChabok <RezaChabok@proton.me>
2024-08-21 21:09:52 +0330

    fix

--- API/API/settings.py
+++ API/API/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)0xd9igmhm_(-=wih%qq0#+j-$fvm9*v8vujrs^vb$jdvct2*f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['badsectorDB.pythonanywhere.com']
+ALLOWED_HOSTS = ['ُtaprepo.pythonanywhere.com', '127.0.0.1']
 # CSRF_TRUSTED_ORIGINS = ['moz-extension://0f2ffc5e-6ee6-44d4-8ab1-8a2a8cdcae3f']
 
 
